2a0307b1f9624ed7ae579ca7819f1d697bbf247b
Ved pandey <ved532pandey@gmail.com>
2021-04-10 11:45:14 +0530

    modified

--- Pig.js
+++ Pig.js
@@ -1,7 +1,25 @@
 class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
+    this.Visibility=255;
+
     this.image = loadImage("sprites/enemy.png");
   }
+display(){
+  
+  console.log(this.body.speed);
+if(this.body.speed<3){
+  super.display();
+}
 
+else {
+  push();
+  this.Visibility=this.Visibility-5;
+  tint(255,this.Visibility);
+  image(this.image,this.body.position.x,this.body.position.y,50,50);
+  pop();
+  World.remove(world,this.body);
+
+}
+}
 };
